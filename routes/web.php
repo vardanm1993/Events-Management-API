b5036410e3df14a82062872dbd1f6be5ff245f7d
vardanm1993 <vardanm1993@gmail.com>
2024-01-15 16:43:07 +0000

    Modified: remove resource controllers

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\AttendeeController;
-use App\Http\Controllers\Api\EventController;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +12,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('events', EventController::class);
-Route::apiResource('events.attendees', AttendeeController::class)
-    ->scoped(['attendee' => 'event']);
+
 
 
 
