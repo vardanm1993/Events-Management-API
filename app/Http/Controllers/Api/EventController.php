6dff01583bd8d46c5537ff2bda3f1a976da295a8
vardanm1993 <vardanm1993@gmail.com>
2024-01-15 18:05:19 +0000

    Modified: add store, update and destroy logic

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Event\StoreEventRequest;
+use App\Http\Requests\Event\UpdateEventRequest;
 use App\Models\Event;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class EventController extends Controller
 {
@@ -19,15 +22,18 @@ class EventController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreEventRequest $request): Event
     {
-        //
+        return Event::create([
+            ...$request->validated(),
+            'user_id' => 1
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Event $event)
+    public function show(Event $event): Event
     {
         return $event;
     }
@@ -35,16 +41,20 @@ class EventController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateEventRequest $request, Event $event): Event
     {
-        //
+         $event->update($request->validated());
+
+         return $event;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event): Response
     {
-        //
+        $event->delete();
+
+        return response(status: 204);
     }
 }
