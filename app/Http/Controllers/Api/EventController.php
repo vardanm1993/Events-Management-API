9a326c3a1e9a386a24205ae5397782d53ad2cbac
vardanm1993 <vardanm1993@gmail.com>
2024-01-17 19:15:49 +0000

    Modified: add api  middleware  rate limiting for event

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -21,8 +21,10 @@ class EventController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:sanctum')->except(['index','show']);
-        $this->authorizeResource(Event::class,'event');
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+        $this->middleware('throttle:api')
+            ->only(['store', 'update', 'destroy']);
+        $this->authorizeResource(Event::class, 'event');
     }
 
     /**
